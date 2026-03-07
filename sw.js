157a4560d6e1c3940bb3f5f8966170e4891e6fff
cheeseiscool12 <jlmitchellgoat@gmail.com>
2026-03-07 16:08:07 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Frederick Tag Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
